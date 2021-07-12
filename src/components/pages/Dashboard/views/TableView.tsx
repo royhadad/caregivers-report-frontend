dae47e23b10a0f0c75a4c4ad9c73ff4c7d425ad2
royhadad98 <roy@roundtrip.co.il>
2021-07-12 15:49:01 +0300

    aggregated the patients for each caregiver

--- src/components/pages/Dashboard/views/TableView.tsx
+++ src/components/pages/Dashboard/views/TableView.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components/macro";
-import { ReactComponent as MedFlytLogo } from "../../../../assets/logo.svg";
-import { ReactComponent as RefreshIcon } from "../../../../assets/refresh.svg";
+import {ReactComponent as MedFlytLogo} from "../../../../assets/logo.svg";
+import {ReactComponent as RefreshIcon} from "../../../../assets/refresh.svg";
 import Center from "../../../shared/Center";
 import Row from "../../../shared/Row";
 import Button from "../components/Button";
 import None from "../components/None";
-import { PrimaryText } from "../components/PrimaryText";
+import {PrimaryText} from "../components/PrimaryText";
 import RefreshIndicator from "../components/RefreshIndicator";
 import TableBlocks from "../components/TableBlocks";
-import { Report } from "../Dashboard";
+import {Report} from "../Dashboard";
+import getAggregatedReport from "../../../../utils/getAggregatedReport";
 
 interface Props {
     report: Report;
@@ -17,9 +18,9 @@ interface Props {
 }
 
 const Header = styled(Center)`
-    height: auto;
-    gap: 32px;
-    margin-bottom: 48px;
+  height: auto;
+  gap: 32px;
+  margin-bottom: 48px;
 `;
 
 const StyledTableView = styled.div`
@@ -37,45 +38,48 @@ const StyledTableView = styled.div`
 const { Table, Tr, Th, Td } = TableBlocks;
 
 const TableView = (props: Props) => {
+    const aggregatedReport = getAggregatedReport(props.report);
+    const {year, caregivers} = aggregatedReport;
+
     return (
         <StyledTableView>
-            <RefreshIndicator isRefreshing={props.isRefreshing} />
+            <RefreshIndicator isRefreshing={props.isRefreshing}/>
             <Header>
-                <MedFlytLogo />
-                <PrimaryText>Year {props.report.year} - caregivers report</PrimaryText>
+                <MedFlytLogo/>
+                <PrimaryText>Year {year} - caregivers report</PrimaryText>
             </Header>
             <Row justifyContent="flex-end">
                 <Button>
-                    <RefreshIcon />
+                    <RefreshIcon/>
                     <span>Refresh</span>
                 </Button>
             </Row>
             <Table>
                 <thead>
-                    <Tr>
-                        <Th>Caregiver name</Th>
-                        <Th>Patients</Th>
-                    </Tr>
+                <Tr>
+                    <Th>Caregiver name</Th>
+                    <Th>Patients</Th>
+                </Tr>
                 </thead>
                 <tbody>
-                    {props.report.caregivers.map((caregiver, idx) => (
-                        <Tr key={idx}>
-                            <Td>{caregiver.name}</Td>
-                            <Td>
-                                {caregiver.patients.length > 0 ? (
-                                    caregiver.patients
-                                ) : (
-                                    <None />
-                                )}
-                            </Td>
-                        </Tr>
-                    ))}
+                {caregivers.map((caregiver, idx) => (
+                    <Tr key={idx}>
+                        <Td>{caregiver.name}</Td>
+                        <Td>
+                            {caregiver.patients.length > 0 ? (
+                                caregiver.patients
+                            ) : (
+                                <None/>
+                            )}
+                        </Td>
+                    </Tr>
+                ))}
                 </tbody>
-                {props.report.caregivers.length === 0 ? (
+                {caregivers.length === 0 ? (
                     <tbody>
-                        <tr>
-                            <td colSpan={2}>No results!</td>
-                        </tr>
+                    <tr>
+                        <td colSpan={2}>No results!</td>
+                    </tr>
                     </tbody>
                 ) : null}
             </Table>
